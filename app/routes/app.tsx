a41dece30bb44d62f5eef70ec6313e427fdf1237
mondragon-ai <angel.mondragon@yahoo.com>
2024-11-10 21:32:55 -0300

    💳 Billing & simple Ui patch

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -21,17 +21,17 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const admin = await authenticate.admin(request);
   const { session, billing } = admin;
 
-  await billing.require({
-    plans: [USAGE_PLAN],
-    isTest: true,
-    onFailure: async () => billing.request({ plan: USAGE_PLAN, isTest: true }),
-  });
-
   // const billingCheck = await billing.require({
   //   plans: [USAGE_PLAN],
   //   isTest: false,
   //   onFailure: async () => billing.request({ plan: USAGE_PLAN, isTest: false }),
   // });
+
+  await billing.require({
+    plans: [USAGE_PLAN],
+    isTest: false,
+    onFailure: async () => billing.request({ plan: USAGE_PLAN, isTest: false }),
+  });
 
   // await fetch(
   //   `${SERVER_BASE_URL}/store/${session.shop}/install/${session.accessToken}`,
